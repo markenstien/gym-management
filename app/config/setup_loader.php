98ef1d9316a8d31c41492b34e4a77ca5973029dc
markenstein <markgonzalespersonal@gmail.com>
2022-12-12 19:40:20 +0800

    all files

---
+++ app/config/setup_loader.php
@@ -0,0 +1,76 @@
+<?php
+    define('SYSTEM_MODE' , $system['mode']);
+
+    define('UI_THEME' , $ui['vendor']);
+
+    define('APP_NAME' , $system['app_name']);
+
+
+    define('DB_PREFIX' , 'hr_');
+
+    switch(SYSTEM_MODE)
+    {
+        case 'local':
+            define('URL' , 'http://dev.gym_mgmt');
+            define('DBVENDOR' , 'mysql');
+            define('DBHOST' , 'localhost');
+            define('DBUSER' , 'root');
+            define('DBPASS' , '');
+            define('DBNAME' , 'th_gym_mgmt');
+
+            define('BASECONTROLLER' , 'AuthController');
+            define('BASEMETHOD' , 'index');
+
+            ini_set('display_errors', 1);
+            ini_set('display_startup_errors', 1);
+            error_reporting(E_ALL);
+        break;
+
+        case 'dev':
+            define('URL' , '');
+            define('DBVENDOR' , '');
+            define('DBHOST' , '');
+            define('DBUSER' , '');
+            define('DBPASS' , '');
+            define('DBNAME' , '');
+
+            define('BASECONTROLLER' , 'Pages');
+            define('BASEMETHOD' , 'index');
+
+            ini_set('display_errors', 1);
+            ini_set('display_startup_errors', 1);
+            error_reporting(E_ALL);
+        break;
+
+        case 'down':
+            define('URL' , '');
+            define('DBVENDOR' , '');
+            define('DBHOST' , '');
+            define('DBUSER' , '');
+            define('DBPASS' , '');
+            define('DBNAME' , '');
+
+            define('BASECONTROLLER' , 'Maintenance');
+            define('BASEMETHOD' , 'index');
+
+            ini_set('display_errors', 1);
+            ini_set('display_startup_errors', 1);
+            error_reporting(E_ALL);
+        break;
+
+        case 'up':
+            define('URL' , '#');
+            define('DBVENDOR' , 'mysql');
+            define('DBHOST' , 'localhost');
+            define('DBUSER' , '#');
+            define('DBPASS' , '#');
+            define('DBNAME' , '#');
+
+            define('BASECONTROLLER' , 'AuthController');
+            define('BASEMETHOD' , 'index');
+            
+            ini_set('display_errors', 1);
+            ini_set('display_startup_errors', 1);
+            error_reporting(E_ALL);
+        break;
+    }
